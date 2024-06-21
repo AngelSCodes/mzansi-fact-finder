2fcdbdba1f12fa82d028f9006ced9bfd8b0ff762
AngelSCodes <158100915+AngelSCodes@users.noreply.github.com>
2024-06-21 14:15:17 +0200

    integrated AI API for functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-function generateFact(event){
-    event.preventDefault();
+function displayFact(response){
 
     new Typewriter('#fact-response', {
-        strings: "Fact about South Africa.",
+        strings: response.data.answer,
         autoStart: true,
         delay: 5,
-        cursor: "💡",
+        cursor: "📖",
       });
+
+}
+
+function generateFact(event){
+    event.preventDefault();
+    
+    let userEnquiry = document.querySelector("#user-enquiry");
+
+    let apiKey = "ta0fa480a5ddb93fe2fa0b479bf3b70o";
+    let prompt = `user question:is there ele ${userEnquiry.value}`;
+    let context = "You are a historian who specialises in South African history and highly equipped with its current affairs and trends. Your mission is to provide users with detailed facts about the country in basic HTML and separate each line with <br />. Please respond to the user's question.Sign the fact with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("Genarating fact");
+    console.log(`prompt: ${prompt}`);
+    console.log(`context: ${context}`);
+
+      axios.get(apiUrl).then(displayFact);
 }
 
 let factRequest = document.querySelector("#fact-generator-form");
